1a56250b84c847f7614bd1f84433f520cb1b069d
Jacob Gonzales <jaggtony@gmail.com>
2022-05-19 19:52:08 -0700

    save for later

---
+++ asyncStart/app.js
@@ -0,0 +1,18 @@
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)  //add tri catch block
+}
+
+getACuteDogPhoto()
+
+
+const http = require('http')
+const fs = require('fs')
+http.createServer((req, res) => {
+  fs.readFile('demofile.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+}).listen(8000)
