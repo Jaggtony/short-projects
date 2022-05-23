8d4c1bb418dbffbaa0aac0d5f966cd3d0507e1e2
Jacob Gonzales <jaggtony@gmail.com>
2022-05-22 18:01:13 -0700

    simple back end

---
+++ coin flip1/main.js
@@ -0,0 +1,25 @@
+document.getElementById("headsButton").addEventListener('click', () =>{
+    let choice = Math.ceil(Math.random()*2)
+    if (choice === 1){
+      choice = "Heads"
+      document.querySelector("img").src="front.png"
+      document.querySelector("p").innerHTML= "You Win"
+    }else{
+      choice = "Tails"
+      document.querySelector("img").src="back.png"
+      document.querySelector("p").innerHTML= "You Lose"
+    }
+  })
+
+  document.getElementById("tailsButton").addEventListener('click', () =>{
+    choice = Math.ceil(Math.random()*2)
+    if (choice === 1){
+      choice = "Tails"
+      document.querySelector("img").src="back.png"
+      document.querySelector("p").innerHTML= "You Win"
+    }else{
+      choice = "Heads"
+      document.querySelector("img").src="front.png"
+      document.querySelector("p").innerHTML= "You Lose"
+    }
+  })
