9e9fe6b722b33c39f64944c9b612158da0bb6f80
Jacob Gonzales <jaggtony@gmail.com>
2022-05-30 17:40:32 -0700

    RPSLS

--- server.js
+++
@@ -1,9 +0,0 @@
-console.log('May Node be with you')
-const express = require('express');
-const app = express();
-app.listen(3000,  function(){
-    console.log('listneing on 3000')
-})
-app.get('/', (req, res) => {
-    res.sendFile('/Users/jaggt/Desktop/repositories/short-projects/index.html')
-  })
